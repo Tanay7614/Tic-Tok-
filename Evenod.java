0df15244f8b99e19385783c29d53d37642e5b74f
Tanay Rathod <tanay7614.com>
2021-10-04 19:58:17 +0530

    1st commit

---
+++ Evenod.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Evenod {
+	public static void main(String[] args) {
+		 System.out.println("enter a no");
+		Scanner sc = new Scanner(System.in);
+		 int p=sc.nextInt();
+		 if(p%2==0) {
+			 System.out.println("No is even no");
+		 }
+		 else {
+			 System.out.println("No is odd");
+		 }
+
+}
+}
