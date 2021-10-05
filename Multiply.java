7d0382a9cf87e74ebcb9bb5ae7d36a2e6f35cf21
Tanay Rathod <tanay7614.com>
2021-10-05 14:39:55 +0530

    intia2

---
+++ Multiply.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Multiply {
+	public static void main(String[] args) {
+		 System.out.println("enter a no");
+		Scanner sc = new Scanner(System.in);
+		 int p=sc.nextInt();
+}
+}
