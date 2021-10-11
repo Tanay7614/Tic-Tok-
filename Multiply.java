2131cc06d280b282424c47c5b0d070508efc388d
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-10-11 20:29:34 +0530

    Delete Multiply.java

--- Multiply.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Multiply {
-	public static void main(String[] args) {
-		 System.out.println("enter a no");
-		Scanner sc = new Scanner(System.in);
-		 int p=sc.nextInt();
-}
-}
